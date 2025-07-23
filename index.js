a9e29874bd017959f39afddaf92a02a1ee71232c
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-23 13:22:01 +0530

    added port

--- index.js
+++ index.js
@@ -3,17 +3,10 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 
-const conncetion = mysql.createConnection({
-  host:process.env.HOST,
-  user :process.env.USER,
-  password:process.env.PASSWORD,
-  database:process.env.DATABASE,
-  port:process.env.PORT
-});
+
 app.listen(process.env.PORT);
 
 app.get("/users", (req, res)=>{
-    conncetion.query("SELECT * FROM userData", (err, result)=>{
-      res.send(result)
-    });
+    
+      res.send({user:"kartikey"})
 });
