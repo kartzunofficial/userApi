e353b6f41415b0957d5b251a9ce379e727bc4560
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-23 12:49:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+
+const conncetion = mysql.createConnection({
+  host:process.env.HOST,
+  user :process.env.USER,
+  password:process.env.PASSWORD,
+  database:process.env.DATABASE,
+  port:process.env.PORT
+});
+app.listen(8080);
+
+app.get("/users", (req, res)=>{
+    conncetion.query("SELECT * FROM userData", (err, result)=>{
+      res.send(result)
+    });
+});
